5282430150b569ce05119a4e021d49e35f6172f2
jkillingit <justin.r.killion@gmail.com>
2020-05-19 01:23:37 +0000

    Weather & News Working

--- app.rb
+++ app.rb
@@ -9,14 +9,36 @@ get "/" do
   lat = 42.0574063
   long = -87.6722787
 
-  units = "imperial" # or metric, whatever you like
-  key = "9f35050b376e19c67d3ec6f8b2d75a3d" # replace this with your real OpenWeather API key
+  units = "imperial"
+  weatherkey = "9f35050b376e19c67d3ec6f8b2d75a3d"
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
-
-  # make the call
+  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{weatherkey}"
   @forecast = HTTParty.get(url).parsed_response.to_hash
+    
+    puts "Current Temperature: #{@forecast["current"]["temp"]} degrees"
+    puts "Current Conditions: #{@forecast["current"]["weather"][0]["description"]}"
+    puts "8 Day Extended forecast:"
+    day_number = 1
+    for day in @forecast["daily"]
+        puts "#{day_number} Out: A high of #{day["temp"]["max"]} and #{day["weather"][0]["description"]}"
+        day_number = day_number + 1
+    end
 
   ### Get the news
+
+  newskey = "a206b7a0d88440aebebbeb610616dc89"
+  url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=#{newskey}"
+  @news = HTTParty.get(url).parsed_response.to_hash
+
+    puts "Top Headlines"
+    article_number = 1
+    for article in @news["articles"]
+        puts "#{article_number}: #{article["title"]}" 
+        puts "By #{article["author"]}"
+        puts "Preview: #{article["content"]}"
+        puts "#{article["urlToImage"]}"
+        puts "Full Article: #{article["url"]}"
+        article_number = article_number + 1
+    end
 end
